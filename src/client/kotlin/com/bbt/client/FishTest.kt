94d1f15bb47b679597ecbb7e6e694d93b5eef4b0
unknown <idkdwij@gmail.com>
2026-07-09 16:24:47 -0400

    main

---
+++ src/client/kotlin/com/bbt/client/FishTest.kt
@@ -0,0 +1,4 @@
+package com.bbt.client
+
+class FishTest {
+}
---
+++ src/client/kotlin/com/bbt/client/FuckJoseClient.kt
@@ -0,0 +1,9 @@
+package com.bbt.client
+
+import net.fabricmc.api.ClientModInitializer
+
+object FuckJoseClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
